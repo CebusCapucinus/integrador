3087fbe18cf76d1049abee72a57b51651691c986
Cerdan, Marcos <cerdanmarcos96@gmail.com>
2026-06-15 13:21:44 -0300

    segundo commit

--- src/main/java/com/mycompany/integrador/entities/Base.java
+++ src/main/java/com/mycompany/integrador/entities/Base.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.integrador.entities;
 
-/**
- *
- * @author marco
- */
-public class Base {
-    
+import java.time.LocalDateTime;
+
+public abstract class Base {
+
+    protected Long id;
+    protected boolean eliminado;
+    protected LocalDateTime createdAt;
+
+    public Base() {
+        this.createdAt = LocalDateTime.now();
+        this.eliminado = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
